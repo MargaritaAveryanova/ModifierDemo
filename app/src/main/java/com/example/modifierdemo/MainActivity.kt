8437fdaca87f1feef849ee774a9fdff664953365
MargaritaAveryanova <120007228+MargaritaAveryanova@users.noreply.github.com>
2026-09-16 12:44:28 +0700

    примнение нескольких модификаторов

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -47,6 +47,8 @@ fun DemoScreen(modifier: Modifier = Modifier) {
         .border(width = 2.dp, color = Color.Red)
         .padding(all = 10.dp)
 
+    val secondModifier = Modifier.height(100.dp)
+
     Column(
         Modifier.padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -56,7 +58,7 @@ fun DemoScreen(modifier: Modifier = Modifier) {
 
     Text(
         "Hello Compose",
-        mymodifier,
+        mymodifier.then(secondModifier),
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
